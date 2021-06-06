eefee982435e39bd567a7a9bb47ebaeffc232ebd
Renato V.G. Ńazário <renatovgnaza@yahoo.com.br>
2021-06-06 18:02:21 -0300

    Comitando alterações nova versão

--- src/main/java/com/project/bootcamp/BootcampApplication.java
+++ src/main/java/com/project/bootcamp/BootcampApplication.java
@@ -20,7 +20,7 @@ public class BootcampApplication {
 								 @Value("${application.version}") String version){
 		return new OpenAPI().info(new Info()
 				.title(description)
-				.version("1.0")
+				.version("2.0")
 				.termsOfService("http://swagger.io/terms")
 				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
